15441af4f561bf997180cf3045aaeb49b8ef74e6
Vicente <vikom.sanchez@gmail.com>
2024-04-19 19:34:18 -0300

    refactor(packages/auth): add RBAC  + refactor(apps/api)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,17 @@
-import { ability } from '@nivo/auth'
+import { defineAbilityFor } from '@nivo/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User') // true
 const userCanDeleteDeleteOtherUsers = ability.can('delete', 'User') // false
 
 const userCannotDeleteOtherUsers = ability.cannot('delete', 'User') // true
 
-console.log(`\n ////// can ///// \n`)
+console.log(`\n////// can ///// \n`)
 
 console.log('userCanInviteSomeoneElse: ' + userCanInviteSomeoneElse)
 console.log('userCanDeleteDeleteOtherUsers: ' + userCanDeleteDeleteOtherUsers)
 
-console.log(`\n ////// cannot ///// \n`)
+console.log(`\n////// cannot ///// \n`)
 
 console.log('userCannotDeleteOtherUsers: ' + userCannotDeleteOtherUsers)
---
+++ packages/auth/src/models/user.ts
@@ -0,0 +1,5 @@
+import { Role } from '../permissions'
+
+export type User = {
+  role: Role
+}
