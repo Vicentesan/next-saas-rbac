9a8064c0ef12f466f2aa6feae04c6f6a2bde67d0
Vicente <vikom.sanchez@gmail.com>
2024-04-19 23:52:37 -0300

    feat(packages/auth): add new permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,25 @@
+import { randomUUID } from 'node:crypto'
+
 import { defineAbilityFor } from '@nivo/auth'
+import { projectSchema } from '@nivo/auth/src/models/project'
 
-const ability = defineAbilityFor({ role: 'MEMBER' })
+const userId = randomUUID()
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User') // true
-const userCanDeleteDeleteOtherUsers = ability.can('delete', 'User') // false
+const ability = defineAbilityFor({ role: 'MEMBER', id: userId })
+const project = projectSchema.parse({ id: randomUUID(), ownerId: userId })
 
-const userCannotDeleteOtherUsers = ability.cannot('delete', 'User') // true
+const userCanCreateProject = ability.can('create', 'Project')
+const userCanUpdateAnyProject = ability.can('update', 'Project') // erro esta aqui... esta retornando true
+const userCanDeleteAnyProject = ability.can('delete', 'Project') // erro esta aqui... esta retornando true
 
-console.log(`\n////// can ///// \n`)
+const userCanUpdateHisOwnProject = ability.can('update', project)
+const userCanDeleteHisOwnProject = ability.can('delete', project)
 
-console.log('userCanInviteSomeoneElse: ' + userCanInviteSomeoneElse)
-console.log('userCanDeleteDeleteOtherUsers: ' + userCanDeleteDeleteOtherUsers)
+console.log('\n/////// can ///////\n')
+console.log(`userCanCreateProject: ${userCanCreateProject}`)
+console.log(`userCanUpdateAnyProject: ${userCanUpdateAnyProject}`) // erro esta aqui... esta retornando true
+console.log(`userCanDeleteAnyProject: ${userCanDeleteAnyProject}`) // erro esta aqui... esta retornando true
+console.log(`userCanUpdateHisOwnProject: ${userCanUpdateHisOwnProject}`)
+console.log(`userCanDeleteHisOwnProject: ${userCanDeleteHisOwnProject}`)
 
-console.log(`\n////// cannot ///// \n`)
-
-console.log('userCannotDeleteOtherUsers: ' + userCannotDeleteOtherUsers)
+console.log('\n/////// cannot ///////\n')
