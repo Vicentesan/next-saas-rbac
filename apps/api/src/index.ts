e6766c93a0306e845a86039b674d80c0ed401d0e
Vicente <vikom.sanchez@gmail.com>
2024-04-20 16:22:19 -0300

    fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -8,18 +8,37 @@ const userId = randomUUID()
 const ability = defineAbilityFor({ role: 'MEMBER', id: userId })
 const project = projectSchema.parse({ id: randomUUID(), ownerId: userId })
 
+const anotherProject = projectSchema.parse({
+  id: randomUUID(),
+  ownerId: randomUUID(),
+})
+
 const userCanCreateProject = ability.can('create', 'Project')
-const userCanUpdateAnyProject = ability.can('update', 'Project') // erro esta aqui... esta retornando true
-const userCanDeleteAnyProject = ability.can('delete', 'Project') // erro esta aqui... esta retornando true
+const userCanUpdateAnyProject = ability.can('update', anotherProject)
+const userCanDeleteAnyProject = ability.can('delete', anotherProject)
 
 const userCanUpdateHisOwnProject = ability.can('update', project)
 const userCanDeleteHisOwnProject = ability.can('delete', project)
 
+const userCannotCreateProject = ability.cannot('create', 'Project')
+const userCannotUpdateAnyProject = ability.cannot('update', anotherProject)
+const userCannotDeleteAnyProject = ability.cannot('delete', anotherProject)
+
+const userCannotUpdateHisOwnProject = ability.cannot('update', project)
+const userCannotDeleteHisOwnProject = ability.cannot('delete', project)
+
 console.log('\n/////// can ///////\n')
+
 console.log(`userCanCreateProject: ${userCanCreateProject}`)
-console.log(`userCanUpdateAnyProject: ${userCanUpdateAnyProject}`) // erro esta aqui... esta retornando true
-console.log(`userCanDeleteAnyProject: ${userCanDeleteAnyProject}`) // erro esta aqui... esta retornando true
+console.log(`userCanUpdateAnyProject: ${userCanUpdateAnyProject}`)
+console.log(`userCanDeleteAnyProject: ${userCanDeleteAnyProject}`)
 console.log(`userCanUpdateHisOwnProject: ${userCanUpdateHisOwnProject}`)
 console.log(`userCanDeleteHisOwnProject: ${userCanDeleteHisOwnProject}`)
 
 console.log('\n/////// cannot ///////\n')
+
+console.log(`userCannotCreateProject: ${userCannotCreateProject}`)
+console.log(`userCannotUpdateAnyProject: ${userCannotUpdateAnyProject}`)
+console.log(`userCannotDeleteAnyProject: ${userCannotDeleteAnyProject}`)
+console.log(`userCannotUpdateHisOwnProject: ${userCannotUpdateHisOwnProject}`)
+console.log(`userCannotDeleteHisOwnProject: ${userCannotDeleteHisOwnProject}`)
