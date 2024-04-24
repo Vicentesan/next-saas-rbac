0e0e4336017f293e174e5f3ec4176075babd2b4b
Vicente <vikom.sanchez@gmail.com>
2024-04-24 14:14:33 -0300

    feat(http): init fastify

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,23 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    ({ body }) => {
+      const { name, email } = body
+
+      return { name, email }
+    },
+  )
+}
