c704b80978d2468fb0381ff35798dba514c0fe99
Vicente <vikom.sanchez@gmail.com>
2024-04-27 17:51:25 -0300

    feat(http/routes): add create acc route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,10 +17,31 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    ({ body }) => {
-      const { name, email } = body
+    async (req, res) => {
+      const { name, email, password } = req.body
 
-      return { name, email }
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail)
+        return res
+          .status(400)
+          .send({ message: 'User with same email already exists.' })
+
+      const hashedPassword = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash: hashedPassword,
+        },
+      })
+
+      return res.status(201).send()
     },
   )
 }
