7354cd0d56408428b54af50d475f510311b479ad
Vicente <vikom.sanchez@gmail.com>
2024-04-29 17:14:59 -0300

    feat(http/routes): add get profile route

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,46 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/me',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Get authenticated user profile',
+        response: {
+          200: z.object({
+            user: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string().email(),
+              avatarUrl: z.string().url().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { sub } = await req.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      })
+
+      if (!user) throw new Error('User not found')
+
+      return res.send({ user })
+    },
+  )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,7 @@ import { env } from '@/env'
 
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
+import { getProfile } from './routes/auth/get-profile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -44,6 +45,7 @@ app.register(fastifyCors)
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app
   .listen({
