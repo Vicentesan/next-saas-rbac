5cb272644b898c520257a89fcbd10c1d228225e0
Vicente <vikom.sanchez@gmail.com>
2024-04-29 18:30:33 -0300

    feat(http/routes): add request password recovery route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -20,7 +20,7 @@ export async function createAccount(app: FastifyInstance) {
           password: z.string().min(6),
         }),
         response: {
-          201: z.object({}),
+          201: z.null(),
         },
       },
     },
---
+++ apps/api/src/http/routes/auth/request-password-recovery.ts
@@ -0,0 +1,47 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecovery(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Request password recovery',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (req, res) => {
+      const { email } = req.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) return res.status(201).send() // we don't want to expose if the email is not registered
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVERY',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // send email
+
+      console.log('Recovery token:' + code)
+
+      return res.status(201).send()
+    },
+  )
+}
